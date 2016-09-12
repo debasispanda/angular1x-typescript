e040d54196629d9ddf3bc9b3f7076aaf5c672750
Debasis Panda <imdebasispanda@gmail.com>
2016-09-12 09:57:42 +0530

    Used angular services
    Used angular default services and data binding.

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,20 +1,34 @@
+export interface IHeader  extends ng.IScope
+{
+    author: string;
+    currentDateTime: Number;
+}
+
 export class HeaderComponent{
 	public author:string;
 	public currentDateTime:Number;
-	constructor(){
-		this.showAlert();
+	public $rootScope: any;
+	static $inject = ["$scope", "$rootScope", "$interval"];
+	constructor($scope: any, $rootScope: any, $interval: any){
+		let self = this;	
+		this.$rootScope = $rootScope;
+			
 		this.author = "Debasis Panda";
-		this.setCurrentTime();		
+		this.currentDateTime = Date.now();
+		$interval(function(){
+			self.setCurrentTime();				
+		}, 1000);	
+		this.$rootScope.name = 'Anagular1x + TypeScript + Webpack';	
+		this.showAlert();		
 	}	
-	showAlert():void{
+	
+	public showAlert():void{
 		alert('Wow, its working fine.');
 	}
-	setCurrentTime():void{
-		let self = this;
-		let dateTime: Date = new Date();
-		this.currentDateTime = dateTime.valueOf();
-		// setTimeout(function(){
-		// 	self.setCurrentTime();
-		// }, 50)
+	public setCurrentTime():void{
+		let self = this, $scope: any;		
+		this.currentDateTime = Date.now();
+		this.$rootScope.message = 'Thank you for using the app.';	
 	}
+	
 }
